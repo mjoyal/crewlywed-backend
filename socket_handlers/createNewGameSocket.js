06a89e3c5d68dea1bf441ae49e89b00a02e37b8c
Will <wdportman@gmail.com>
2020-11-24 13:56:44 -0800

    set up CreateNewHost

--- socket_handlers/createNewGameSocket.js
+++ socket_handlers/createNewGameSocket.js
@@ -1,12 +1,23 @@
-const { createNewGame } = require('../db/helpers/createNewGame');
+const { createNewGame, createNewHost } = require('../db/helpers/createNewGame');
 
 const createNewGameSocket = (socket, db) => {
   socket.on('createNewGame', createNewGameData => {
     createNewGame(createNewGameData, db)
     .then(data => {
-      const gameData = data.rows[0].id;
-      console.log(`New game #${gameData} created`);
-      socket.emit('createNewGameReturn', gameData)
+      const gameID = data.rows[0].id;
+      console.log(`New game #${gameID} created`);
+      socket.emit('createNewGameReturn', gameID)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  });
+
+  socket.on('createNewHost', createNewHostData => {
+    createNewHost(createNewHostData, db)
+    .then(data => {
+      const hostID = data.rows[0].id;
+      console.log(`New host #${hostID} created`);
     })
     .catch(error => {
       console.log(error);
