094bf5db4c4159ba0565ab2c30db971a0a3b58bf
Will <wdportman@gmail.com>
2020-11-24 21:07:39 -0800

    Set up functionality to check if game code is for an existing, non-ended, and non-full game

--- db/helpers/joinGame.js
+++ db/helpers/joinGame.js
@@ -1,5 +1,32 @@
-const joinGame = function(joinGameData, db) {
-
+const checkIfGameIsLive = function (joinGameData, db) {
+  const query = `
+    SELECT id
+    FROM sessions
+    WHERE code = $1
+    AND finished_at IS NULL
+  ;`;
+  const params = [joinGameData.gameCode];
+  return db.query(query, params)
 };
 
-module.exports = { joinGame };
+const checkIfGameIsFull = function(sessionID, db) {
+  const query = `
+    SELECT count(*)
+    FROM players
+      JOIN sessions ON sessions.id = players.session_id
+    WHERE players.session_id = $1
+  ;`;
+  const params = [sessionID];
+  return db.query(query, params)
+};
+
+
+const joinGame = function(joinGameData, db) {
+  // const query = `
+
+  // ;`;
+  // const params = [];
+  // return db.query(query, params)
+};
+
+module.exports = { checkIfGameIsLive, checkIfGameIsFull, joinGame };
--- socket_handlers/joinGameSocket.js
+++ socket_handlers/joinGameSocket.js
@@ -1,7 +1,29 @@
-const { joinGame } = require('../db/helpers/joinGame');
+const { checkIfGameIsLive, checkIfGameIsFull, joinGame } = require('../db/helpers/joinGame');
 
 const joinGameSocket = (socket, db) => {
-
+  socket.on('joinGame', joinGameData => {
+    checkIfGameIsLive(joinGameData, db)
+    .then(data => {
+      if (data.rows[0]) {
+        // console.log("Session ID:", data.rows[0])
+        sessionID = data.rows[0].id;
+        checkIfGameIsFull(sessionID, db)
+        .then(data => {
+          // console.log("# of players:", data.rows[0].count)
+          numPlayers = data.rows[0].count;
+          if (numPlayers < 8) {
+            console.log("Success! Game exists, is live, and is not full")
+          } else {
+            console.log("This game is full")
+          }
+        })
+        .catch(e => console.error(e.stack))
+      } else {
+        console.log("This is not a currently-active game");
+      }
+    })
+    .catch(e => console.error(e.stack))
+  })
 };
 
 module.exports = { joinGameSocket };
