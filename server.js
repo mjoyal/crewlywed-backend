618e24e3073ce145c858ebab7dd61bd121c0d35b
Will <wdportman@gmail.com>
2020-11-25 22:51:34 -0800

    Rename lobbySocket to createLobbySocket

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const {getAvatarSocket} = require('./socket_handlers/getAvatarSocket');
 const {createNewGameSocket} = require('./socket_handlers/createNewGameSocket');
 const {joinGameSocket} = require('./socket_handlers/joinGameSocket');
 const {chatSocket} = require('./socket_handlers/chatSocket');
-const {lobbySocket} = require('./socket_handlers/lobbySocket');
+const {createLobbySocket} = require('./socket_handlers/createLobbySocket');
 
 // PG database client/connection setup:
 const { Pool } = require('pg');
@@ -51,7 +51,7 @@ io.on('connection', socket => {
     //APP:
     createNewGameSocket(socket, db);
     joinGameSocket(socket, db);
-    lobbySocket(socket, db);
+    createLobbySocket(socket, db);
 });
 
 
