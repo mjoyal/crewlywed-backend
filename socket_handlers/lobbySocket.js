50231adca1331b587058a4d50c5bf9733523dfe6
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-11-26 04:52:52 +0000

    added lobbysocket && getPlayers + getGameCode helpers

---
+++ socket_handlers/lobbySocket.js
@@ -0,0 +1,28 @@
+
+const { getPlayers } = require('../db/helpers/getPlayers');
+const { getGameCode } = require('../db/helpers/getGameCode');
+
+const lobbySocket = (socket, db) => {
+  socket.on('createHostLobby', (gameID) => {
+    getPlayers(gameID, db)
+      .then(data => {
+        const lobbyInfo = data.rows[0];
+        lobbyInfo["gameID"] = gameID;
+        socket.emit('hostLobbyReturn', lobbyInfo);
+      })
+      .catch(error => {
+        console.error(`Cannot retreive players for gameID ${gameID}`);
+      });
+
+     getGameCode(gameID, db)
+      .then(data => {
+        socket.emit('gameCodeReturn', data.rows[0]);
+      })
+      .catch(error => {
+        console.error(`Cannot retreive game code for gameID ${gameID}`)
+      })
+  })
+};
+
+
+module.exports = { lobbySocket };
