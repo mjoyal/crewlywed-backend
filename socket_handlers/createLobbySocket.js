f1be70a9e719bf4e1ae13defb52655aada43fe7e
Will <wdportman@gmail.com>
2020-11-26 12:08:45 -0800

    Set up join room functionality upon create game / join game

--- socket_handlers/createLobbySocket.js
+++ socket_handlers/createLobbySocket.js
@@ -1,6 +1,12 @@
 const { getGameCode, getPlayers } = require('../db/helpers/createLobby');
 
-const createLobbySocket = (socket, db) => {
+const createLobbySocket = (socket, db, io) => {
+
+  socket.on("joinRoom", (roomCode) => {
+    socket.join(roomCode);
+    console.log(`Room ${roomCode} joined`);
+    io.in(roomCode).emit('allPlayersData', 'HELLOOOOOO');
+  });
 
 };
 
