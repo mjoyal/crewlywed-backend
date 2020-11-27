291900900e1215f55bc5154a287a0f25aede8cc4
Will <wdportman@gmail.com>
2020-11-26 20:22:51 -0800

    Implement updateFinishedAt to update sessions.finished_at when a game ends (i.e. when final score is shown)

--- db/helpers/manageGame.js
+++ db/helpers/manageGame.js
@@ -7,11 +7,21 @@ const updateStartedAt = function(gameID, db) {
   ;`;
   const params = gameID;
   return db.query(query, [params])
+};
 
+const updateFinishedAt = function(gameID, db) {
+  const query = `
+    UPDATE sessions
+    SET finished_at = NOW()
+    WHERE sessions.id = $1
+    RETURNING started_at
+  ;`;
+  const params = gameID;
+  return db.query(query, [params])
 };
 
 const createRounds = function(gameID, db) {
 
 };
 
-module.exports = { updateStartedAt, createRounds };
+module.exports = { updateStartedAt, updateFinishedAt, createRounds };
--- socket_handlers/manageGameSocket.js
+++ socket_handlers/manageGameSocket.js
@@ -1,24 +1,32 @@
-const { updateStartedAt, createRounds } = require('../db/helpers/manageGame');
+const { updateStartedAt, updateFinishedAt, createRounds } = require('../db/helpers/manageGame');
 
 const manageGameSocket = (socket, db, io) => {
 
   socket.on('startGame', (userProfile) => {
-    const gameRoom = userProfile.code
-    const gameID = userProfile.session_id
+    const gameRoom = userProfile.code;
+    const gameID = userProfile.session_id;
+
     io.in(gameRoom).emit('startGameReturn');
+
     // Update the session's started_at field in the sessions table:
     updateStartedAt(gameID, db)
     .then(data => {
-      console.log('Started_at updated for session:', data.rows[0]);
+      console.log(`Started_at updated for session ${gameID}:`, data.rows[0]);
     });
+
     // Create rows for the session in the rounds table:
     createRounds(gameRoom, db)
     // .then(data => {
     //   console.log(data);
     // });
 
+    // setTimeout to transition to end of game (this is a placeholder for now; will be updated later):
     setTimeout(() => {
       io.in(gameRoom).emit('finalScore');
+      updateFinishedAt(gameID, db)
+      .then(data => {
+        console.log(`Finished_at updated for session ${gameID}:`, data.rows[0]);
+      });
     }, 3000)
 
   });
