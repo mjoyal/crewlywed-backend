d5cac609916d6488fa060f50dd89b455915a7f25
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-11-27 20:01:41 +0000

    added await state for on click of choose answer

--- socket_handlers/manageGameSocket.js
+++ socket_handlers/manageGameSocket.js
@@ -7,6 +7,7 @@ const manageGameSocket = (socket, db, io) => {
     const gameID = userProfile.session_id;
 
     io.in(gameRoom).emit('startGameReturn');
+    
 
     // Update the session's started_at field in the sessions table:
     updateStartedAt(gameID, db)
@@ -39,17 +40,15 @@ const manageGameSocket = (socket, db, io) => {
     })
 
 
-
-
     // setTimeout to transition to end of game (this is a placeholder for now; will be updated later):
-    setTimeout(() => {
-      io.in(gameRoom).emit('finalScore');
-      updateFinishedAt(gameID, db)
-      .then(data => {
-        console.log(`Finished_at updated for session ${gameID}:`, data.rows[0]);
-      });
-    }, 3000)
- 
+    // setTimeout(() => {
+    //   io.in(gameRoom).emit('finalScore');
+    //   updateFinishedAt(gameID, db)
+    //   .then(data => {
+    //     console.log(`Finished_at updated for session ${gameID}:`, data.rows[0]);
+    //   });
+    // }, 3000)
+
 
   });
 
