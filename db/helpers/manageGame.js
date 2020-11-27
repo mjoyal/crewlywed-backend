e030b65e3401620f588e96bb5515aba733ab42ec
Will <wdportman@gmail.com>
2020-11-26 19:41:03 -0800

    Set up DB helper functions triggered by startGame event: updateStartedAt & createRounds

---
+++ db/helpers/manageGame.js
@@ -0,0 +1,9 @@
+const updateStartedAt = function(gameRoom) {
+
+};
+
+const createRounds = function(gameRoom) {
+
+};
+
+module.exports = { updateStartedAt, createRounds };
--- socket_handlers/manageGameSocket.js
+++ socket_handlers/manageGameSocket.js
@@ -1,7 +1,19 @@
+const { updateStartedAt } = require('../db/helpers/manageGame');
+
 const manageGameSocket = (socket, db, io) => {
 
   socket.on('startGame', (gameRoom) => {
     io.in(gameRoom).emit('startGameReturn');
+    // Update the session's started_at field in the sessions table:
+    updateStartedAt(gameRoom)
+    .then(data => {
+      console.log(data);
+    });
+    // Create rows for the session in the rounds table:
+    createRounds(gameRoom)
+    .then(data => {
+      console.log(data);
+    });
 
     setTimeout(() => {
       io.in(gameRoom).emit('finalScore');
