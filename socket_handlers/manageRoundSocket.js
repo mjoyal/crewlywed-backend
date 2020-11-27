e4817eddbda3068b1aae410dc736f2c20eca390f
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-11-27 22:05:23 +0000

    added roundscore into the loop

--- socket_handlers/manageRoundSocket.js
+++ socket_handlers/manageRoundSocket.js
@@ -15,7 +15,12 @@ socket.on('startGame', (hostInfo) => {
           setTimeout(() => {
             // send roundOver info with new question etc
             // however this is set to finalScore for now.
-            io.in(gameRoom).emit('roundOver');
+            io.in(gameRoom).emit('roundScore');
+
+            setTimeout(() => {
+              io.in(gameRoom).emit('roundOver');
+            }, 5000);
+
           }, 5000)
 
         }, 5000);
