497f18ff7fb52bf46a50e44c5dacb60af1b62c92
Will <wdportman@gmail.com>
2020-11-26 19:21:07 -0800

    Update game codes in DB seed file to lowercase

--- db/seeds/01_sessions.sql
+++ db/seeds/01_sessions.sql
@@ -1,5 +1,5 @@
 INSERT INTO sessions (created_at, started_at, finished_at, code, rounds_per_player)
 VALUES
-('2020-11-11 00:00:00', NULL, NULL, 'ABCD', 1),
-('2020-11-18 01:00:00', '2020-11-18 01:00:30', '2020-11-18 01:45:00', 'EFGH', 1)
+('2020-11-11 00:00:00', NULL, NULL, 'abcd', 1),
+('2020-11-18 01:00:00', '2020-11-18 01:00:30', '2020-11-18 01:45:00', 'efgh', 1)
 ;
