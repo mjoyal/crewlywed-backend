61f651529524dd6e531dbc663726a67a421fa893
Will <wdportman@gmail.com>
2020-11-30 13:57:46 -0800

    Organize/make more concise manageRoundsSocket_roundTransitions

--- socket_handlers/manageRoundsSocket_roundTransitions.js
+++ socket_handlers/manageRoundsSocket_roundTransitions.js
@@ -1,69 +1,41 @@
 const { getSubmissions, getRevealData, getScoreData} = require('../db/helpers/manageRounds');
 
 const manageRoundsSocket_roundTransitions = (socket, db, io) => {
+  socket.on('startGame', (hostInfo) => {
+    const gameRoom = hostInfo.code;
 
-  // manage roundStates
-socket.on('startGame', (hostInfo) => {
-  const gameRoom = hostInfo.code;
-
-  // during this timer, we show the 'ANSWER' page
-  socket.on('newRound', (roundID) => {
-
-    setTimeout(() => {
-      // timer is up for answering, show choose page
-      // get all potential choices (submissions) for round X in session_id Y
-
-      const submissionInfo = {
-        round: roundID,
-        session: hostInfo.session_id
-      };
-      getSubmissions(submissionInfo, db)
-        .then((submissionData) => {
-          io.in(gameRoom).emit('choosePage', submissionData.rows);
-        })
-        .catch(() => {
-          console.error(`can not find submissions for round ${submissionInfo.round} and session ${submissionInfo.session}`)
-        });
-
-
-          // once players are on the choosePage, set new timer
-          // for how long they have to choose the correct answer
-
-
-          setTimeout(() => {
-            // show the reveal page for time to see the results
-            getRevealData(roundID, db)
-            .then((data) => {
-              io.in(gameRoom).emit('revealPage', data.rows);
-
-            });
-
-
-
-
-
+    socket.on('newRound', (roundID) => {
+      // ST#1: ANSWER:
+      setTimeout(() => {
+        const submissionInfo = {
+          round: roundID,
+          session: hostInfo.session_id
+        };
+        getSubmissions(submissionInfo, db)
+          .then((submissionData) => {
+            io.in(gameRoom).emit('choosePage', submissionData.rows);
+          })
+            // ST #2: CHOOSE:
             setTimeout(() => {
-              // send roundOver info with new question etc
-              // however this is set to finalScore for now.
-              getScoreData(hostInfo.session_id, db)
-              .then(data => {
-                io.in(gameRoom).emit('roundScore', data.rows);
-
+              getRevealData(roundID, db)
+              .then((data) => {
+                io.in(gameRoom).emit('revealPage', data.rows);
               });
-
+              // ST #3: REVEAL:
               setTimeout(() => {
-                io.in(gameRoom).emit('roundOver');
-              }, 2000); // ROUNDSCORE
-
-            }, 8000) // REVEAL
-
-          }, 10000); // CHOOSE
-
-      }, 10000); // ANSWER
-  });
-});
+                getScoreData(hostInfo.session_id, db)
+                .then(data => {
+                  io.in(gameRoom).emit('roundScore', data.rows);
+                });
+                // ST #4: ROUNDSCORE:
+                setTimeout(() => {
+                  io.in(gameRoom).emit('roundOver');
+                }, 2000); // ST#4: ROUNDSCORE
+              }, 8000) // ST#3: REVEAL
+            }, 10000); // ST#2: CHOOSE
+          }, 10000); // ST#1: ANSWER
+      });
+    });
 };
-
-
 
 module.exports = { manageRoundsSocket_roundTransitions };
