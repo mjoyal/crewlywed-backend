102531ca68c987d9e77bbc5e717703d571e3eaa0
Will <wdportman@gmail.com>
2020-11-30 14:25:28 -0800

    rewrite useRoundLoop and manageRoundsSocket_roundTransitions to eliminate nested setTimeouts in favor of socket events for each step

--- socket_handlers/manageRoundsSocket_roundTransitions.js
+++ socket_handlers/manageRoundsSocket_roundTransitions.js
@@ -4,8 +4,8 @@ const manageRoundsSocket_roundTransitions = (socket, db, io) => {
   socket.on('startGame', (hostInfo) => {
     const gameRoom = hostInfo.code;
 
-    socket.on('newRound', (roundID) => {
-      // ST#1: ANSWER:
+    socket.on('newRound', roundID => {
+      // ST #1: Show ANSWER until ST expires, then show CHOOSE:
       setTimeout(() => {
         const submissionInfo = {
           round: roundID,
@@ -15,27 +15,36 @@ const manageRoundsSocket_roundTransitions = (socket, db, io) => {
           .then((submissionData) => {
             io.in(gameRoom).emit('choosePage', submissionData.rows);
           })
-            // ST #2: CHOOSE:
-            setTimeout(() => {
-              getRevealData(roundID, db)
-              .then((data) => {
-                io.in(gameRoom).emit('revealPage', data.rows);
-              });
-              // ST #3: REVEAL:
-              setTimeout(() => {
-                getScoreData(hostInfo.session_id, db)
-                .then(data => {
-                  io.in(gameRoom).emit('roundScore', data.rows);
-                });
-                // ST #4: ROUNDSCORE:
-                setTimeout(() => {
-                  io.in(gameRoom).emit('roundOver');
-                }, 2000); // ST#4: ROUNDSCORE
-              }, 8000) // ST#3: REVEAL
-            }, 10000); // ST#2: CHOOSE
-          }, 10000); // ST#1: ANSWER
+        }, 10000);
       });
+
+    socket.on('newRevealPage', () => {
+       // ST #2: Show CHOOSE until ST expires. then show REVEAL:
+       setTimeout(() => {
+        getRevealData(roundID, db)
+        .then((data) => {
+          io.in(gameRoom).emit('revealPage', data.rows);
+        });
+      }, 10000);
     });
+
+    socket.on('newRoundScorePage', () => {
+       // ST #3: Show REVEAL until ST expires, then show ROUNDSCORE:
+       setTimeout(() => {
+        getScoreData(hostInfo.session_id, db)
+        .then(data => {
+          io.in(gameRoom).emit('roundScore', data.rows);
+        });
+      }, 8000)
+    });
+
+    socket.on('newAnswerPage', () => {
+      // ST #4: Show ROUNDSCORE until ST expires, then show ANSWER:
+      setTimeout(() => {
+        io.in(gameRoom).emit('roundOver');
+      }, 2000);
+    }); //can we also figure out finalscore here?
+  })
 };
 
 module.exports = { manageRoundsSocket_roundTransitions };
