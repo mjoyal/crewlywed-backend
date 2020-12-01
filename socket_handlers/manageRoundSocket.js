77d24edd613dbdecde548da4d3019f8d7ee57a6f
Will <wdportman@gmail.com>
2020-11-30 20:50:04 -0800

    Try to fix bug where only 1st submission goes through

--- socket_handlers/manageRoundSocket.js
+++ socket_handlers/manageRoundSocket.js
@@ -2,8 +2,12 @@ const { insertAnswer, getAwaitAnswerData, checkIfEveryoneHasSubmitted, insertCho
 
 const manageRoundSocket = (socket, db, io) => {
   socket.on('startGame', (hostInfo) => {
+
     const gameRoom = hostInfo.code;
     const gameID = hostInfo.session_id;
+
+    // let answerDone = false;
+    // let chooseDone = false;
 
     // FUNCTIONS:
     // Function for Timer #1:
@@ -36,42 +40,31 @@ const manageRoundSocket = (socket, db, io) => {
 
     // TRANSITION EVENT LISTENERS AND TIMERS:
     // Timer values (in milliseconds):
-    const answerDuration = 8000;
-    const chooseDuration = 5000;
-    const revealDuration = 3000;
-    const roundscoreDuration = 3000;
+    const answerDuration = 10000;
+    const chooseDuration = 10000;
+    const revealDuration = 5000;
+    const roundscoreDuration = 5000;
 
     // Timer #1: Show ANSWER / AWAIT until timer expires OR until everyone submits, then show CHOOSE:
     socket.on('newRound', roundID => {
-
-      let done = false;
-
       const answerTimer = setTimeout(() => {
           showChoose(hostInfo, roundID, db);
         }, answerDuration);
-
-        if(true && !done) {
-          showChoose(hostInfo, roundID, db);
-          clearTimeout(answerTimer);
-          done = true;
-        };
-
-      });
+      // if(answerDone) {
+      //   showChoose(hostInfo, roundID, db);
+      //   clearTimeout(answerTimer);
+      // };
+    });
 
     // Timer #2: Show CHOOSE / AWAIT until timer expires OR until everyone chooses. then show REVEAL:
     socket.on('newRevealPage', roundID => {
-
-      let done = false;
-
       const chooseTimer = setTimeout(() => {
         showReveal(roundID, db);
       }, chooseDuration);
-
-      if(true && !done) {
-        showReveal(roundID, db);
-        clearTimeout(chooseTimer);
-        done = true;
-      };
+      // if(chooseDone) {
+      //   showReveal(roundID, db);
+      //   clearTimeout(chooseTimer);
+      // };
     });
 
     // Timer #3: Show REVEAL until timer expires, then show ROUNDSCORE:
@@ -86,10 +79,10 @@ const manageRoundSocket = (socket, db, io) => {
       const roundscoreTimer = setTimeout(() => {
         io.in(gameRoom).emit('roundOver');
       }, roundscoreDuration);
-    }); //can we also figure out finalscore here?
+    }); //can we also figure out finalscore here? *****************
 
 
-    // USER INPUT LISTENERS AND SOCKETS:
+    // USER INPUT LISTENERS:
     // Receive user submission:
     socket.on('thisUserSubmitted', (userAnswerInfo) => {
       // Add user submission into submissions table
@@ -98,26 +91,22 @@ const manageRoundSocket = (socket, db, io) => {
           console.log("Submissions.id:", data.rows);
         })
           .then (() => {
-            const roundID = userAnswerInfo.round;
-            const gameID = userAnswerInfo.userProfile.session_id;
-            // Check if everyone has submitted; if so, we will skip the the next screen:
-            checkIfEveryoneHasSubmitted(roundID, gameID, db)
-              .then(data => {
-                const submittedStatus = data.rows[0].submitted_status;
-                console.log("submittedStatus:", submittedStatus);
-                if (submittedStatus) {
-                  io.in(gameRoom).emit("submittedStatusTrue")
-                  console.log("SUBMITTED STATUS = TRUE")
-                } else {
-                  return getAwaitAnswerData(userAnswerInfo.userProfile.session_id, userAnswerInfo.round, db)
-                    .then((data) => {
-                      console.log("awaitData", data.rows);
-                      io.in(userAnswerInfo.userProfile.code).emit('awaitData', data.rows);
-                    })
-                  }
-                })
-              })
-      });
+            return getAwaitAnswerData(userAnswerInfo.userProfile.session_id, userAnswerInfo.round, db)
+          })
+            .then((data) => {
+              console.log("awaitData", data.rows);
+              io.in(userAnswerInfo.userProfile.code).emit('awaitData', data.rows);
+            })
+    });
+      // const roundID = userAnswerInfo.round;
+      // const gameID = userAnswerInfo.userProfile.session_id;
+      // // Check if everyone has submitted; if so, we will skip the the next screen:
+      // checkIfEveryoneHasSubmitted(roundID, gameID, db)
+      //   .then(data => {
+      //     const submittedStatus = data.rows[0].submitted_status;
+      //     console.log("submittedStatus:", submittedStatus);
+      //   })
+
 
     // Receive user choice:
     socket.on('thisUserChose', (userChoiceInfo) => {
@@ -127,25 +116,21 @@ const manageRoundSocket = (socket, db, io) => {
           console.log(`added choice ${userChoiceInfo.choice} for chooser ${userChoiceInfo.userProfile.id}`)
         })
           .then(() => {
-            const roundID = userChoiceInfo.round;
-            const gameID = userChoiceInfo.userProfile.session_id;
-            // Check if everyone has chosen; if so, we will skip the the next screen:
-            checkIfEveryoneHasChosen(roundID, gameID, db)
-              .then(data => {
-                const chosenStatus = data.rows[0].chosen_status;
-                console.log('chosenStatus:', chosenStatus);
-                if (chosenStatus) {
-                  console.log("CHOSEN STATUS = TRUE")
-                } else {
-                  return getAwaitChoiceData(userChoiceInfo.userProfile.session_id, userChoiceInfo.round, db)
-                    .then((data) => {
-                      console.log("awaitData", data.rows);
-                      io.in(userChoiceInfo.userProfile.code).emit('awaitData', data.rows);
-                    })
-                  }
-                })
-              })
-      });
+            return getAwaitChoiceData(userChoiceInfo.userProfile.session_id, userChoiceInfo.round, db)
+          })
+            .then((data) => {
+              console.log("awaitData", data.rows);
+              io.in(userChoiceInfo.userProfile.code).emit('awaitData', data.rows);
+            })
+    });
+
+      // const roundID = userChoiceInfo.round;
+      // const gameID = userChoiceInfo.userProfile.session_id;
+      // // Check if everyone has chosen; if so, we will skip the the next screen:
+      // checkIfEveryoneHasChosen(roundID, gameID, db)
+      //   .then(data => {
+      //     const chosenStatus = data.rows[0].chosen_status;
+      //     console.log('chosenStatus:', chosenStatus);
 
   })
 };
